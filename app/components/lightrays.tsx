a05eced37cb0676ee6e1f87ae5d05f2e78d822b3
Romeo-04 <jatolentino@fit.edu.ph>
2025-12-25 19:02:27 +0800

    added assets, lightray effect

---
+++ app/components/lightrays.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const lightrays = () => {
+  return (
+    <div>lightrays</div>
+  )
+}
+
+export default lightrays
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,9 @@
-// No need to import React in Next.js 13 and later
-import Hello from "./components/hello";
+import React from 'react'
 
-async function Home () {
-  const response = await fetch('https://jsonplaceholder.typicode.com/albums');
-  if(!response.ok) throw new Error('Failed to fetch data');
-
-  const albums = await response.json();
-
+const page = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
-      {albums.map((album: { id: number; title: string }) => (
-        <div key={album.id} className="border p-4 rounded shadow">
-          <h3 className="text-xl font-bold mb-2">Album {album.id}</h3>
-          <p>{album.title}</p>
-        </div>
-      ))}
-    </div>
+    <h1>page</h1>
   )
 }
 
-
-export default Home;
+export default page
